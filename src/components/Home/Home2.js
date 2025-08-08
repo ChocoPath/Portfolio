967dcc98927e256e5210b64a0d5832b003e21f1a
Worsley <worsley.quaye@qubole.com>
2025-08-08 11:40:11 +0000

    Update LinkedIn URL to correct profile
    - Update LinkedIn URL from /quayeworsley/ to /worsley-quaye/
    - Apply changes to both Home2.js and Footer.js components
    - Ensure consistent social media links across the portfolio

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -85,7 +85,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/quayeworsley/"
+                  href="https://www.linkedin.com/in/worsley-quaye/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
